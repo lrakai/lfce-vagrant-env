227f8ec5ffad529a8260fe39cdd23d8d2e113eac
lrakai <lmrakai@gmail.com>
2019-05-25 18:44:43 -0600

    Fix root sshing

--- provisioners/box.sh
+++ provisioners/box.sh
@@ -1,10 +1,10 @@
 nodes=("node1" "node2")
 
 for i in $(seq 0 $(( ${#nodes[@]} - 1 )) ); do
-    if ! grep ${nodes[$i]}  /etc/ssh/ssh_config ; then
-        echo >> /etc/ssh/ssh_config <<EOF
+    if ! grep -q ${nodes[$i]} /etc/ssh/ssh_config ; then
+        cat >> /etc/ssh/ssh_config <<EOF
 
-Host node
+Host ${nodes[$i]}
     HostName ${nodes[$i]}
     Port 22
     IdentityFile /vagrant/files/insecure_private_key
--- provisioners/common.sh
+++ provisioners/common.sh
@@ -3,7 +3,7 @@ yum install -y epel-release
 if [ ! -d /root/.ssh ]; then
     mkdir /root/.ssh
 fi
-if ! grep insecure /root/.ssh/authorized_keys ; then
+if ! test -f /root/.ssh/authorized_keys || ! grep -q insecure /root/.ssh/authorized_keys ; then
     insecure_public_key=$(ssh-keygen -y -f /vagrant/files/insecure_private_key)
     echo "$insecure_public_key vagrant insecure public key" >> /root/.ssh/authorized_keys
 fi
@@ -12,7 +12,7 @@ nodes=("node1" "node2")
 ips=("10.0.0.101" "10.0.0.102")
 
 for i in $(seq 0 $(( ${#nodes[@]} - 1 )) ); do
-    if ! grep ${nodes[$i]} /etc/hosts ; then
+    if ! grep -q ${nodes[$i]} /etc/hosts ; then
         echo "${ips[$i]} ${nodes[$i]} " >> /etc/hosts
     fi
 done
