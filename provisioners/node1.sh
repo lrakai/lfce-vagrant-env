56ae2e342c44d320c22b124c679624117806cec5
lrakai <lmrakai@gmail.com>
2019-07-06 18:33:04 -0600

    Silence curl

--- provisioners/node1.sh
+++ provisioners/node1.sh
@@ -1,2 +1,2 @@
 cd /tmp 
-curl -L http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-3.10.0-957.21.3.el7.x86_64.rpm -O
+curl -L http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-3.10.0-957.21.3.el7.x86_64.rpm -O -s
